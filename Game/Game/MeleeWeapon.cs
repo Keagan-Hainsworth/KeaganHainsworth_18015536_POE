33f4a24768c6238b81bc2b73a73beb05011d18bc
Keagan Hainsworth <52012634+Keagan-Hainsworth@users.noreply.github.com>
2020-11-30 12:06:37 +0200

    update 1

---
+++ Game/Game/MeleeWeapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class MeleeWeapon : Weapon
+    {
+        public enum Types
+        {
+            Longsword,
+            Dagger
+        }
+
+        public int Damage = ;
+        public int Ranage = 1;
+        public int Durability = ;
+        public int 
+       
+    }
+}
---
+++ Game/Game/Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    abstract class Weapon : Item
+    {
+        public int Damage
+        {
+            get
+            set
+                
+        }
+
+        public int Range
+        {
+            get { return }
+            set { }
+        }
+
+        public int Durability
+        {
+            get { }
+            set { }
+        }
+
+        public int Cost
+        {
+            get { }
+            set { }
+        }
+
+        public string Weapon_Type
+        {
+            get { }
+            set 
+        }
+    }
+}
